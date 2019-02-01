537909ee390a297f50b29beb1a0bd7da3864d1f0
Emily Wagner <emilywagner598@gmail.com>
2019-02-01 00:46:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,8 +14,8 @@ def load_library(filepath)
   lib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(filepath, emot)
+  lib = load_library(filepath)
 end
 
 def get_english_meaning
