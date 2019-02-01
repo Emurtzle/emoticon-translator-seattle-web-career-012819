954d58fe514ae90cb077ca706d6ee731b69f74bb
Emily Wagner <emilywagner598@gmail.com>
2019-02-01 00:44:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(filepath)
 
   yml.each do |k,v|
     lib["get_meaning"][v[1]] = k
-    lib["get_emoticon"][v[0]] = k
+    lib["get_emoticon"][v[0]] = v[1]
   end
   binding.pry
   lib
